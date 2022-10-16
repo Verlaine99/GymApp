e50f735fe5bc1c6dde151ad4b6fd2019c63dd47a
Verlaine99 <camilo.achury.mt@usa.edu.co>
2022-10-15 23:36:44 -0500

    Version 12

--- src/main/java/com/example/demo/crudrepository/MachineCrudRepository.java
+++ src/main/java/com/example/demo/crudrepository/MachineCrudRepository.java
@@ -1,7 +1,6 @@
 package com.example.demo.crudrepository;
 
 import com.example.demo.entities.Machine;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 public interface MachineCrudRepository extends CrudRepository<Machine, Integer>{
